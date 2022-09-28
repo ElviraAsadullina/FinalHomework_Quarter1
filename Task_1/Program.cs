9621bf96d4c15e7cbd1afcc2ae5c36b8840eff24
Elvira <eliasufa@yahoo.com>
2022-09-29 01:58:29 +0500

    написан воид по формированию нового массива

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿void DeleteLongElements (string[] values, int limit)
+{
+    for(int i = 0; i < values.Length; i++)
+    {
+        char[] characters = values[i].ToCharArray();
+        if(characters.Length <= limit)
+        {
+            Console.Write("'" + values[i] + "'");
+            if(i < values.Length - 2)
+            {
+                Console.Write("," + " ");
+            } 
+        }
+    }
+}
+string[] CurrentCharacters = {"123", "object", "New1", "@12", "examples", "0-0", "preview%", "no", ")", "master32"};
+Console.WriteLine("Новый массив из строк с длиной символов <= 3:");
+DeleteLongElements (CurrentCharacters, 3);
