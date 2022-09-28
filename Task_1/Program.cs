cf950514eb98a13dd46ca0c40eae86f6b30b4329
Elvira <eliasufa@yahoo.com>
2022-09-29 04:30:40 +0500

    Переписан метод создания нового массива

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,18 +1,28 @@
-﻿void GetShortArray (string[] values, int limit)
+﻿void GetShortArray (string[] StartValues, string[] EndValues, int limit)
 {
-    for(int i = 0; i < values.Length; i++)
+    for(int i = 0, j = 0; i < StartValues.Length && j < EndValues.Length; i++, j++)
     {
-        char[] characters = values[i].ToCharArray();
+        char[] characters = StartValues[i].ToCharArray();
         if(characters.Length <= limit)
         {
-             Console.Write("'" + values[i] + "'");
-             if(i < values.Length - 2)
-             {
-                 Console.Write("," + " ");
-             } 
+            EndValues[j] = StartValues[i];
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == null) continue;
+        Console.Write("'" + array[i] + "'");
+        if(i < array.Length - 2)
+        {
+            Console.Write(", ");
         }
     }
 }
 string[] CurrentCharacters = {"123", "object", "New1", "@12", "examples", "0-0", "preview%", "no", ")", "master32"};
+string[] NewCharacters = new string[CurrentCharacters.Length];
 Console.WriteLine("Новый массив из строк с длиной символов <= 3:");
-GetShortArray (CurrentCharacters, 3);
+GetShortArray (CurrentCharacters, NewCharacters, 3);
+PrintArray(NewCharacters);
